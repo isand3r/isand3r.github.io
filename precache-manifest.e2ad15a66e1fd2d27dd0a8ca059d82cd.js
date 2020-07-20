8218d68a1f0a5337f468b6bc0a49a6d229a1f39d
Isaiah Thiessen <isaiah.thiessen@telus.com>
2020-07-20 11:47:27 -0700

    Updates

---
+++ precache-manifest.e2ad15a66e1fd2d27dd0a8ca059d82cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd75b041efe518a8a78902800505ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da7656eb1c1206e0469",
+    "url": "/static/js/2.a0c3dfa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0c3dfa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee011a83ef652289db57",
+    "url": "/static/js/main.379f9561.chunk.js"
+  },
+  {
+    "revision": "300f765783bb4fb42ae6",
+    "url": "/static/js/runtime-main.1a30709f.js"
+  }
+]);
