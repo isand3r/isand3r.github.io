8882d56e589800cd8853b25cd3196c74a771a083
Isaiah Thiessen <isaiah.thiessen@telus.com>
2020-07-20 11:37:38 -0700

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return(
+    <div className='ui one column centered grid'>
+      <div className='ui large header centered row'>Under Contruction :)</div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
