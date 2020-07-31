75f7637daac207eebcb3b19380d49c7ec51a772a
Isaiah Thiessen <isaiah.thiessen@telus.com>
2020-07-31 11:20:52 -0700

    fix: post

---
+++ README.md
@@ -0,0 +1,4 @@
+TODO:
+- add routing (refreshing sends you back to 'home')
+- add post archiving
+- make posts hideable
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,9 +8,8 @@ const posts = [
   },
   {
     title: 'Tamagochi',
-    body: `A colleague shared this with me yesterday: https://www.kickstarter.com/projects/flipper-devices/flipper-zero-tamagochi-for-hackers.
-It only took me a couple minutes to decide to back it. Feb 1st feels a long way away but I can't wait to test it out!
-`,
+    body: <div> A colleague shared <a href="https://www.kickstarter.com/projects/flipper-devices/flipper-zero-tamagochi-for-hackers">this</a> with me yesterday.
+It only took me a couple minutes to decide to back it. Feb 1st feels a long way away but I can't wait to test it out! </div>,
     date: 'July 31, 2020'
   }
 ]
