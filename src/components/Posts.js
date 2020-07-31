8eea6ac6707629edbada5338ac54535258fa294f
Isaiah Thiessen <isand3r@users.noreply.github.com>
2020-07-31 11:07:52 -0700

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,6 +5,13 @@ const posts = [
     title: 'First post!',
     body: 'This is a test blog post!',
     date: 'July 22 2020'
+  },
+  {
+    title: 'Tamagochi',
+    body: `A colleague shared this with me yesterday: https://www.kickstarter.com/projects/flipper-devices/flipper-zero-tamagochi-for-hackers.
+It only took me a couple minutes to decide to back it. Feb 1st feels a long way away but I can't wait to test it out!
+`,
+    date: 'July 31, 2020'
   }
 ]
 
