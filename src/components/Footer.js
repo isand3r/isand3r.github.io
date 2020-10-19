7b17b3aff924a75c21c99ad7018395926be2c052
Isaiah Thiessen <isaiah.thiessen@telus.com>
2020-10-19 13:10:17 -0700

    fix: footer disclaimer displays properly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ import GitHub from '@material-ui/icons/GitHub'
 
 export default function Footer () {
   return (
-    <BottomNavigation style={{width: '100%', position: 'fixed', bottom: 0}}>
-      <BottomNavigationAction>
-        <Typography style={{ fontStyle: 'italic', fontSize: '10'}}>This site uses javascript but will never track you.</Typography>
-      </BottomNavigationAction>
-      <BottomNavigationAction label="Github" icon={<a style={{color: 'blue'}} href="https://github.com/isand3r"><GitHub /></a>} />
-      <BottomNavigationAction label="LinkedIn" icon={<a style={{color: 'blue'}} href="https://www.linkedin.com/in/isaiah-thiessen"><LinkedIn /></a>} />
-    </BottomNavigation>
+    <div style={{width: '100%', position: 'fixed', bottom: 0}}>
+      <Typography style={{ textAlign: 'center', fontStyle: 'italic', fontSize: '10', color: 'grey'}}>This site uses javascript but will never track you.</Typography>
+      <BottomNavigation>
+        <BottomNavigationAction label="Github" icon={<a style={{color: 'blue'}} href="https://github.com/isand3r"><GitHub /></a>} />
+        <BottomNavigationAction label="LinkedIn" icon={<a style={{color: 'blue'}} href="https://www.linkedin.com/in/isaiah-thiessen"><LinkedIn /></a>} />
+      </BottomNavigation>
+    </div>
   )
 }
