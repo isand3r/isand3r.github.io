8b3e51003cf3aaa9896f7e33f5ccfb654625158d
Isaiah Thiessen <isaiah.thiessen@telus.com>
2021-01-14 15:08:41 -0800

    chore: links open in new tab, added source code link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ import GitHub from '@material-ui/icons/GitHub'
 export default function Footer () {
   return (
     <div style={{width: '100%', position: 'fixed', bottom: 0}}>
-      <Typography style={{ textAlign: 'center', fontStyle: 'italic', fontSize: '10', color: 'grey'}}>This site uses javascript but will never track you.</Typography>
+      <Typography style={{ textAlign: 'center', fontStyle: 'italic', fontSize: '10', color: 'grey'}}>This site uses javascript but will never track you. <a href="https://github.com/isand3r/isand3r.github.io" target="_blank">source</a></Typography>
       <BottomNavigation>
-        <BottomNavigationAction label="Github" icon={<a style={{color: 'blue'}} href="https://github.com/isand3r"><GitHub /></a>} />
-        <BottomNavigationAction label="LinkedIn" icon={<a style={{color: 'blue'}} href="https://www.linkedin.com/in/isaiah-thiessen"><LinkedIn /></a>} />
+        <BottomNavigationAction label="Github" icon={<a style={{color: 'blue'}} href="https://github.com/isand3r" target="_blank"><GitHub /></a>} />
+        <BottomNavigationAction label="LinkedIn" icon={<a style={{color: 'blue'}} href="https://www.linkedin.com/in/isaiah-thiessen" target="_blank"><LinkedIn /></a>} />
       </BottomNavigation>
     </div>
   )
